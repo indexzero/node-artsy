f99163e6d79923edddc722aeca08e9361bce02b2
Jarrett Cruger <jcrugzz@gmail.com>
2014-08-17 19:08:38 -0400

    [api] genese, genome, artwork endpoints

--- endpoints/artist.js
+++ endpoints/artist.js
@@ -7,13 +7,14 @@
  */
 function Artist (api) {
   this.send = api.send.bind(api);
+  this.qs = api.querystring;
   this.api = api;
 }
 
-
 /**
- * Get an artist by name
+ * Get an artist by id `first-last`
  *
+ * @param {String} str Artists id
  * @param {Object} options Optional options.
  * @param {function} fn The callback.
  * @returns {Assign}
@@ -28,6 +29,126 @@ Artist.prototype.get = function (args) {
     options,
     args.fn
   );
-}
+};
+
+/**
+ * Return a list of the artists artwork
+ *
+ * @param {String} str Artists name
+ * @param {Object} options Optional options.
+ * @param {function} fn The callback.
+ * @returns {Assign}
+ * @api public
+ */
+Artist.prototype.artwork = function (args) {
+  args = this.api.args(arguments);
+
+  var options = args.options || {};
+  return this.send(
+    ['artist', args.str, 'artworks.json'],
+    options,
+    args.fn
+  );
+};
+
+/**
+ * Return a more detailed list of artwork
+ *
+ * @param {String} str Artists name
+ * @param {Object} options Optional options.
+ * @param {function} fn The callback.
+ * @returns {Assign}
+ * @api public
+ */
+Artist.prototype.artworkAll = function (args) {
+  args = this.api.args(arguments);
+
+  var options = args.options || {};
+  return this.send(
+    ['artist', args.str, 'artworks', 'all.json'],
+    options,
+    args.fn
+  );
+};
+
+/**
+ * Return an artists genome
+ *
+ * @param {String} str Artists name
+ * @param {Object} options Optional options.
+ * @param {function} fn The callback.
+ * @returns {Assign}
+ * @api public
+ */
+Artist.prototype.genome = function (args) {
+  args = this.api.args(arguments);
+
+  var options = args.options || {};
+  return this.send(
+    ['artist', args.str, 'genome.json'],
+    options,
+    args.fn
+  );
+};
+
+/**
+ * Return an artists genome map
+ *
+ * @param {String} str Artists name
+ * @param {Object} options Optional options.
+ * @param {function} fn The callback.
+ * @returns {Assign}
+ * @api public
+ */
+Artist.prototype.genomeMap = function (args) {
+  args = this.api.args(arguments);
+
+  var options = args.options || {};
+  return this.send(
+    ['artist', args.str, 'genome', 'map.json'],
+    options,
+    args.fn
+  );
+};
+
+/**
+ * Return an artists genes
+ *
+ * @param {String} str Artists name
+ * @param {Object} options Optional options.
+ * @param {function} fn The callback.
+ * @returns {Assign}
+ * @api public
+ */
+Artist.prototype.genes = function (args) {
+  args = this.api.args(arguments);
+
+  var options = args.options || {};
+  return this.send(
+    ['artist', args.str, 'genome', 'genes.json'],
+    options,
+    args.fn
+  );
+};
+
+/**
+ * Get an artists followers on Artsy
+ *
+ * @param {String} str Artists name
+ * @param {Object} options Optional options.
+ * @param {function} fn The callback.
+ * @returns {Assign}
+ * @api public
+ */
+Artist.prototype.followers = function (args) {
+  args = this.api.args(arguments);
+
+  var options = args.options || {};
+  return this.send(
+    ['artist', args.str, 'follow', 'users.json'],
+    options,
+    args.fn
+  );
+};
 
 module.exports = Artist;
