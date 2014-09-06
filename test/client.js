d46a6e996230a1cdab07d5d33d5cff1cacfee731
Jarrett Cruger <jcrugzz@gmail.com>
2014-09-06 14:08:20 -0400

    [fix] remove method

--- test/client.js
+++ test/client.js
@@ -5,13 +5,8 @@ var config = require('../dist/artsy-api');
 
 var client = new Artsy({ token: config.xapp });
 
-client.artists.list(function (err, output) {
-  if (err) return console.error(err);
-  console.dir(output);
-});
-
 // We might want to accept this with a space instead?
-client.artists.get('andy-warhol', function (err, output) {
+client.artist.get('andy-warhol', function (err, output) {
   if (err) return console.error(err);
 
   console.dir(output);
